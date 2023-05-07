5add1b6d14992a896f141b3b6ef3f52f20eeea2b
SecretPasta <boris.shoostin@gmail.com>
2023-05-07 21:27:46 +0300

    Finished initilization method

--- HW2/q1.py
+++ HW2/q1.py
@@ -19,6 +19,8 @@ class Cell:
 class Grid:
     def __init__(self, inited_cells: list[Cell]) -> None:
         self.grid = [[Cell(Coord(i, j), False, ' ') for j in range(3)] for i in range(3)]
+        for cell in inited_cells:
+            self.grid[cell.coord.x][cell.coord.y] = cell
 
     def print_grid_debug(self) -> None:
         for row in self.grid:
@@ -30,12 +32,13 @@ class Grid:
         print(f"-----------------")
         for row in self.grid:
             for cell in row:
-                print(f"| {cell.symbol} |" , end= ' ')
+                print(f"| {cell.symbol} |", end=' ')
             print(f"\n-----------------")
 
 
 def main():
-    grid = Grid()
+    cells = [Cell(Coord(0, 2), True, 'X'), Cell(Coord(1, 1), True, 'X'), Cell(Coord(2, 0), True, 'O')]
+    grid = Grid(cells)
     grid.print_grid()
 
 
