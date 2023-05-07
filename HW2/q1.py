0cc0859e3d78b60f32f34caefdea5672c5d0eae8
SecretPasta <boris.shoostin@gmail.com>
2023-05-07 21:13:55 +0300

    Added HW2 Question 1

---
+++ HW2/q1.py
@@ -0,0 +1,43 @@
+class Coord:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+
+class Cell:
+    def __init__(self, coord: Coord, is_safe: bool, symbol: str) -> None:
+        if coord is not None and is_safe is not None and str is not None:
+            self.coord = coord
+            self.is_safe = is_safe
+            self.symbol = symbol
+        else:
+            self.coord = coord
+            self.is_safe = False
+            self.symbol = ''
+
+
+class Grid:
+    def __init__(self, inited_cells: list[Cell]) -> None:
+        self.grid = [[Cell(Coord(i, j), False, ' ') for j in range(3)] for i in range(3)]
+
+    def print_grid_debug(self) -> None:
+        for row in self.grid:
+            for cell in row:
+                print(f"({cell.coord.x}, {cell.coord.y}): {cell.symbol}", end=" ")
+            print()
+
+    def print_grid(self) -> None:
+        print(f"-----------------")
+        for row in self.grid:
+            for cell in row:
+                print(f"| {cell.symbol} |" , end= ' ')
+            print(f"\n-----------------")
+
+
+def main():
+    grid = Grid()
+    grid.print_grid()
+
+
+if __name__ == '__main__':
+    main()
